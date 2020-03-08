757417a75b7ead61840f2d3351d8aab9c369a173
Saurabh Thombre <M1049064@mindtree.com>
2020-03-08 13:23:17 +0530

    src commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dockerDemo';
+  constructor(private httpClient: HttpClient) { }
+  text = 'hello';
+  name = '';
+  age = '';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+  updateUser() {
+    const url = 'http://localhost:8080/addOrUpdateUser/' + this.name + '/' + this.age;
+    this.httpClient.post(url, { responseType: 'json' })
+      .subscribe(
+        (data: any) => {
+          alert('User details are saved successfully');
+        }
+      );
+  }
 }
+
